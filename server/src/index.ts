b1adf1e953d60d22dd712c27ec04f23fe5256db5
John Kreitlow <863023+radium-v@users.noreply.github.com>
2023-07-18 16:16:35 -0700

    make the server port configurable

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,8 @@
 import { app } from "./app.js";
 import "./api.js";
 
-app.listen(3000, () => {
-    console.log("Server listening on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
 });
